2e4a027de7d8ed6b9832e60f6af0fe685a919e50
fuzzyJess <jrundle99@yahoo.com>
2022-11-03 08:54:38 +0000

    Comments component created and rendered within Review component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,12 @@
+const Comments = (review_id) => {
+
+
+    return (
+        <section id="comments">
+            Comments to go here
+            {console.log(review_id, " <review id from props?")}
+        </section>
+    )
+}
+
+export default Comments
