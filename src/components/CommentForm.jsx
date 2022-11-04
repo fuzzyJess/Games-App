9a0e32feb7b9116bd5cbe857ab986fff5cf50f88
fuzzyJess <jrundle99@yahoo.com>
2022-11-04 11:11:55 +0000

    disable button to prevent user entering an empty comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -5,39 +5,44 @@ import * as api from "../Api";
 const CommentForm = ({ review_id, setComments }) => {
 
   const [addedComment, setAddedComment] = useState("");
+  const [err, setErr] = useState(null);
+  
 
   const handleSubmit = (event) => {
     event.preventDefault();
     
     api.postComment(addedComment, review_id, "tickle122").then((res) => {
+     
         setComments((curComments) => {
-
-            // resets text field
-            document.getElementById('form_body').value='';
-            //resets value held in addedComment
-            setAddedComment("");
+                     
             // updates comments on page
-            return [res, ...curComments]
+            return [res, ...curComments]})
 
+             //resets value held in addedComment            
+            setAddedComment("");
+        }).catch((err) => {
+          setErr(err)
         })
-    })
   };
 
   const handleOnchange = (event) => {
     setAddedComment(event.target.value)
   }
 
+  if (err) return <p>{err}</p>;
+  const isEmpty = addedComment.length === 0;
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Leave a comment</legend>
 
         <label htmlFor="form_body">Enter your comment</label>
-        <input onChange={handleOnchange} id="form_body" type="text" />
+        <textarea onChange={handleOnchange} value={addedComment} id="form_body" type="text" />
         <p>
-          <button type="submit">
+          <button disabled={isEmpty} type="submit">
             submit comment
           </button>
+          {isEmpty && <span>Please enter a comment</span>}
         </p>
       </fieldset>
     </form>
