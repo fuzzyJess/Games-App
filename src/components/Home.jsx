81ad488bbbe11e0897750b2b3d76c21efab38fdb
fuzzyJess <jrundle99@yahoo.com>
2022-11-13 09:17:28 +0000

    add content to home page and update nav bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
 const Home = () => {
     return (
       <>
-        <p>Welcome to Game Reviews!</p>
+        <main>
+        <p>At Game Reviews we are devoted to getting people 
+          together to play brilliant games.</p>
+
+        <img id="cover_img" src="https://images.unsplash.com/photo-1563941402830-3bae42b67b38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWVlcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"></img> 
+
+        <p>On our site you'll find reviews for a real range 
+          of different games. Our writers have reviewed the 
+          classics like Monopoly & Twister as well as huge 
+          strategy games like Scythe. Please feel free to leave 
+          us comments on reviews, we would love to hear which are 
+          your favourites.
+        </p>
+        </main>
       </>
     )
   }
--- src/components/Nav_bar.jsx
+++ src/components/Nav_bar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
-    return <nav><Link className="link" to={'/'}>Home</Link> <Link className="link" to={'/reviews'}>Reviews</Link> <Link className="link" to={'/categories'}>Categories</Link> Sign in</nav>
+    return <nav><Link className="link" to={'/'}>Home</Link> <Link className="link" to={'/reviews'}>Reviews</Link> <Link className="link" to={'/categories'}>Categories</Link> <Link className="link" to={'/users'}>Sign in</Link></nav>
 }
 
 export default NavBar
